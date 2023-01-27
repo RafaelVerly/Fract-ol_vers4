1300dae9afccf462974ae35a654a883b422485dc
RafaelVerly <75439305+RafaelVerly@users.noreply.github.com>
2023-01-26 23:02:03 -0300

    Add files via upload

--- events.c
+++ events.c
@@ -6,10 +6,9 @@
 /*   By: rverly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 20:58:28 by rverly            #+#    #+#             */
-/*   Updated: 2023/01/21 22:29:37 by rverly           ###   ########.fr       */
+/*   Updated: 2023/01/26 19:24:31 by rverly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "./include/fractol.h"
 
 int	closed(int keycode, t_d *d)
@@ -21,35 +20,25 @@ int	closed(int keycode, t_d *d)
 		exit (0);
 	}
 	if (keycode == 124)
-		d->move_x += d->coef;
+		d->move_x += (0.6 * 0.7 / d->zoom);
 	if (keycode == 123)
-		d->move_x -= d->coef;
+		d->move_x -= (0.6 * 0.7 / d->zoom);
 	if (keycode == 126)
-		d->move_y += d->coef;
+		d->move_y += (0.6 * 0.7 / d->zoom);
 	if (keycode == 125)
-		d->move_y -= d->coef;
+		d->move_y -= (0.6 * 0.7 / d->zoom);
 	fractol(d);
 	return (0);
 }
 
 int	scroll_mouse(int keycode, int x, int y, t_d *d)
 {
-	t_img	img_aux;
-
 	(void)x;
 	(void)y;
-	img_aux = d->img;
 	if (keycode == 4)
-	{
 		d->zoom *= 0.8;
-		d->coef *= 1.2;
-	}
 	if (keycode == 5)
-	{
 		d->zoom *= 1.2;
-		if(d->coef 
-		d->coef *= 0.8;
-	}
 	fractol(d);
 	return (0);
 }
